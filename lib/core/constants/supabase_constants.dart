fb0808c969a0c6e4dc8636a0d4f4a59d13cb4d89
Habitra Dev <habitra@app.dev>
2026-03-04 11:30:47 +0000

    🔄 Yeni Supabase hesabına geçiş (jbpokrgowwpapmirgigl)
    - Supabase URL ve anon key güncellendi
    - Tüm tablolar, RLS, trigger'lar ve fonksiyonlar yeni projede kuruldu
    - Güvenlik kontrolleri temiz (0 uyarı)

--- lib/core/constants/supabase_constants.dart
+++ lib/core/constants/supabase_constants.dart
@@ -1,6 +1,6 @@
 class SupabaseConstants {
-  static const String url = 'https://nttnbvgqfakpxfuefpyn.supabase.co';
-  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50dG5idmdxZmFrcHhmdWVmcHluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NDEyMzYsImV4cCI6MjA4ODExNzIzNn0.3qVPIgFB8-k40J4Tr6lmWZig-c4nWDdwbKueJHTn7vs';
+  static const String url = 'https://jbpokrgowwpapmirgigl.supabase.co';
+  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpicG9rcmdvd3dwYXBtaXJnaWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2MTg3NDMsImV4cCI6MjA4ODE5NDc0M30.dc_97RIB0DikQuld_b99mprY3aAfo64fjm5of6ReBPY';
   
   // TODO: Google Cloud Console'dan OAuth Client ID'leri alıp buraya ekleyin
   static const String googleClientId = 'YOUR_GOOGLE_CLIENT_ID';
